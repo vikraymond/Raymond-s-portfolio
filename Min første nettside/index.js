caf06c62bdb6adaee71fce5fd4982f5898f7e592
vikraymond <vikraymond5rgmail.com>
2024-04-03 11:11:13 +0200

    Redigert kodene.

--- Min første nettside/index.js
+++ Min første nettside/index.js
@@ -1,110 +1,94 @@
 let img1 = document.querySelector("#bilde1");
-
 let bilde1 = document.getElementById("bilde1");
 bilde1.addEventListener("click", function () {
 img1.classList.toggle("bilde1_stor");
 })
 
 let img2 = document.querySelector("#bilde2");
-
 let bilde2 = document.getElementById("bilde2");
 bilde2.addEventListener("click", function () {
 img2.classList.toggle("bilde2_stor");
 })
 
 let img3 = document.querySelector("#bilde3");
-
 let bilde3 = document.getElementById("bilde3");
 bilde3.addEventListener("click", function () {
 img3.classList.toggle("bilde3_stor");
 })
 
 let img4 = document.querySelector("#bilde4");
-
 let bilde4 = document.getElementById("bilde4");
 bilde4.addEventListener("click", function () {
 img4.classList.toggle("bilde4_stor");
 })
 
 let img5 = document.querySelector("#bilde5");
-
 let bilde5 = document.getElementById("bilde5");
 bilde5.addEventListener("click", function () {
 img5.classList.toggle("bilde5_stor");
 })
 
 let img6 = document.querySelector("#bilde6");
-
 let bilde6 = document.getElementById("bilde6");
 bilde6.addEventListener("click", function () {
 img6.classList.toggle("bilde6_stor");
 })
 
 let img7 = document.querySelector("#bilde7");
-
 let bilde7 = document.getElementById("bilde7");
 bilde7.addEventListener("click", function () {
 img7.classList.toggle("bilde7_stor");
 })
 
 let img8 = document.querySelector("#bilde8");
-
 let bilde8 = document.getElementById("bilde8");
 bilde8.addEventListener("click", function () {
 img8.classList.toggle("bilde8_stor");
 })
 
 let img9 = document.querySelector("#bilde9");
-
 let bilde9 = document.getElementById("bilde9");
 bilde9.addEventListener("click", function () {
 img9.classList.toggle("bilde9_stor");
 })
 
 let img10 = document.querySelector("#bilde10");
-
 let bilde10 = document.getElementById("bilde10");
 bilde10.addEventListener("click", function () {
 img10.classList.toggle("bilde10_stor");
 })
 
 let img11 = document.querySelector("#bilde11");
-
 let bilde11 = document.getElementById("bilde11");
 bilde11.addEventListener("click", function () {
 img11.classList.toggle("bilde11_stor");
 })
 
 let img12 = document.querySelector("#bilde12");
-
 let bilde12 = document.getElementById("bilde12");
 bilde12.addEventListener("click", function () {
 img12.classList.toggle("bilde12_stor");
 })
 
 let img13 = document.querySelector("#bilde13");
-
 let bilde13 = document.getElementById("bilde13");
 bilde13.addEventListener("click", function () {
 img13.classList.toggle("bilde13_stor");
 })
 
 let img14 = document.querySelector("#bilde14");
-
 let bilde14 = document.getElementById("bilde14");
 bilde14.addEventListener("click", function () {
 img14.classList.toggle("bilde14_stor");
 })
 
 let img15 = document.querySelector("#bilde15");
-
 let bilde15 = document.getElementById("bilde15");
 bilde15.addEventListener("click", function () {
 img15.classList.toggle("bilde15_stor");
 })
 
 let img16 = document.querySelector("#bilde16");
-
 let bilde16 = document.getElementById("bilde16");
 bilde16.addEventListener("click", function () {
 img16.classList.toggle("bilde16_stor");
