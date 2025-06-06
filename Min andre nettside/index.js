cbeb7032a5c2f3263cd454a1240fc3a597e73599
Lexi303 <dankovicki@gmai.com>
2025-06-06 10:44:50 +0200

    Lagt til antall vinn/tap/uavgjort

--- Min andre nettside/index.js
+++ Min andre nettside/index.js
@@ -30,19 +30,37 @@ function selectPapir() {
 
 papirButton.addEventListener("click", selectPapir);
 
+const displayVinn = document.querySelector("#Vinn");
+const displayTap = document.querySelector("#Tap");
+const displayUavgjort = document.querySelector("#Uavgjort");
+
+let Vinn = 0;
+let Tap = 0;
+let Uavgjort = 0;
+
 function maskinValg() {
     let computerNumber = choices [Math.floor (Math.random () * 3)];
     displayDittValg.innerHTML = dittValg;
     displayMaskinValg.innerHTML = computerNumber;
     if (dittValg === computerNumber) {
         displayResults.innerHTML = "Det er uavgjort!";
-    } else if (computerNumber === "stein" && dittValg === "papir") {
+        Uavgjort += 1;
+        displayUavgjort.innerHTML = "Uavgjort="+Uavgjort;
+    } else if (computerNumber === "Stein" && dittValg === "Papir") {
         displayResults.innerHTML = "Du vinner!";
-    } else if (computerNumber === "papir" && dittValg === "saks") {
+        Vinn += 1;   
+        displayVinn.innerHTML = "Vinn="+Vinn;
+    } else if (computerNumber === "Papir" && dittValg === "Saks") {
         displayResults.innerHTML = "Du vinner!";
-    } else if (computerNumber === "saks" && dittValg === "stein") {
+        Vinn += 1;
+        displayVinn.innerHTML = "Vinn="+Vinn;
+    } else if (computerNumber === "Saks" && dittValg === "Stein") {
         displayResults.innerHTML = "Du vinner!";
+        Vinn += 1;
+        displayVinn.innerHTML = "Vinn="+Vinn;
     } else {
         displayResults.innerHTML = "Du taper!"
+        Tap += 1;
+        displayTap.innerHTML = "Tap="+Tap;
     }
 }
